acdd3f55a02649b4e06dace1a4b70955383d0a9e
toto <toto.techa@gmail.com>
2020-12-06 21:52:19 +0000

    set timezone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    太郎
                 </div>
 
                 <div class="links">
